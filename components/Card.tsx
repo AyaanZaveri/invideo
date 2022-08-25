fb9d35bb7c7d09248b5f9ac0621c762d6838868f
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-08-25 15:53:35 -0400

    Images

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,28 +1,48 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Card = ({ videoData }: any) => {
+
+  // const [channelData, setChannelData] = useState<any>()
+
+  // const getChannelData = (id: string) => {
+  //   axios.get(`https://inv.vern.cc/api/v1/channels/${id}`).then(res => setChannelData(res.data))
+  // };
+
+  // useEffect(() => {
+  //   getChannelData(videoData?.authorId)
+  // }, [])
+
   return (
     <div>
-      {videoData.type == "channel" ? (
-        <div className="w-72">
-          <img
-            src={
-              videoData?.authorThumbnails?.length > 0
-                ? "https:" + videoData?.authorThumbnails[videoData?.authorThumbnails.length - 1]?.url
-                : "https://dummyimage.com/1280x720/fff/aaa"
-            }
-            alt=""
-          />
-        </div>
-      ) : videoData.type == "video" ? (
-        <div className="w-72">
+      {videoData.type == "video" ? (
+        <div className="w-64 flex flex-col">
           <img
             src={
               videoData?.videoThumbnails?.length > 0
                 ? videoData?.videoThumbnails[0]?.url
                 : "https://dummyimage.com/1280x720/fff/aaa"
             }
+            className="rounded-lg"
             alt=""
+          />
+          <div className="flex-">
+
+          </div>
+        </div>
+      ) : videoData.type == "channel" ? (
+        <div className="w-64">
+          <img
+            src={
+              videoData?.authorThumbnails?.length > 0
+                ? "https:" +
+                  videoData?.authorThumbnails[
+                    videoData?.authorThumbnails.length - 1
+                  ]?.url.replace("s88", "s176")
+                : "https://dummyimage.com/1280x720/fff/aaa"
+            }
+            alt=""
+            className="rounded-full w-24"
           />
         </div>
       ) : null}
