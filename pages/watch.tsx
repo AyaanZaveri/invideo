911ae48d21da4388297b919f5edc8baf8f40ee46
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-08-26 18:29:30 -0400

    MPD

--- pages/watch.tsx
+++ pages/watch.tsx
@@ -9,7 +9,7 @@ const Watch = () => {
   const [videoData, setVideoData] = useState<any>();
   const [streams, setStreams] = useState<any>([]);
 
-  const baseUrl = "https://pa.il.ax";
+  const baseUrl = "https://api-piped.mha.fi";
 
   const getVideoData = (id: string) => {
     axios.get(`${baseUrl}/streams/${id}`).then((res) => setVideoData(res.data));
@@ -25,15 +25,32 @@ const Watch = () => {
 
   useEffect(() => {
     setStreams([...videoData?.audioStreams, ...videoData?.videoStreams]);
+    console.log(videoData);
   }, [videoData]);
+
+  let uri: any
 
   const convert = async () => {
     await console.log(streams);
 
-    const uri = await DashUtils.generate_dash_file_from_formats(
+    const dash = await DashUtils.generate_dash_file_from_formats(
       streams,
       videoData?.duration
     );
+
+    uri = "data:application/dash+xml;charset=utf-8;base64," + btoa(dash);
+
+    const url = new URL(uri);
+    const proxyURL = new URL(videoData?.proxyUrl);
+    let proxyPath = proxyURL.pathname;
+    if (proxyPath.lastIndexOf("/") === proxyPath.length - 1) {
+      proxyPath = proxyPath.substring(0, proxyPath.length - 1);
+    }
+    url.searchParams.set("host", url.host);
+    url.protocol = proxyURL.protocol;
+    url.host = proxyURL.host;
+    url.pathname = proxyPath + url.pathname;
+    uri = url.toString();
 
     await console.log(uri);
   };
