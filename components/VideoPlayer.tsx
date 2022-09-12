8358312b39311a5d955aaf987bb1438f53527c64
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-09-11 20:08:10 -0400

    Check

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -73,10 +73,12 @@ const VideoPlayer = ({
     if (typeof player.currentTime === "function" && player) {
       setInterval(() => {
         sponsors.map((sponsor: any) => {
-          if (
-            player?.currentTime().toFixed(0) == sponsor.segment[0].toFixed(0)
-          ) {
-            player?.currentTime(sponsor.segment[1]);
+          if (player) {
+            if (
+              player?.currentTime().toFixed(0) == sponsor.segment[0].toFixed(0)
+            ) {
+              player?.currentTime(sponsor.segment[1]);
+            }
           }
         });
       }, 1000);
