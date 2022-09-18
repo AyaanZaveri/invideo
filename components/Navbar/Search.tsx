aaf0b65ce62ea42bbc22becedcaaefc9098b9477
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-09-18 19:52:26 -0400

    Cog Icon

--- components/Navbar/Search.tsx
+++ components/Navbar/Search.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import React, { useEffect, useRef, useState } from "react";
-import { HiOutlineSearch } from "react-icons/hi";
+import { HiOutlineSearch, HiCog } from "react-icons/hi";
 import onClickOutside from "react-onclickoutside";
 
 const Search = () => {
@@ -51,20 +51,29 @@ const Search = () => {
     <div>
       <div className="flex items-center justify-center flex-col">
         <div className="bg-white/75 backdrop-blur-md relative w-full h-16 flex items-center justify-center flex-row">
-          <img draggable="false" src="/logo.svg" className="p-4 h-16 select-none absolute top-0 left-0" alt="" />
+          <img
+            onClick={() => router.push("/")}
+            draggable="false"
+            src="/logo.svg"
+            className="p-4 h-16 hover:cursor-pointer select-none absolute left-0"
+            alt=""
+          />
           <div className="relative rounded-md shadow-sm w-6/12">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
               <HiOutlineSearch className="text-gray-500 sm:text-sm" />
             </div>
             <form onSubmit={(e) => handleSearch(e)}>
               <input
-                className="focus:ring-orange-500 focus:border-orange-500 block w-full pl-8 pr-12 sm:text-sm border-stone-300 rounded-md transition"
+                className="focus:ring-orange-500 focus:border-orange-500 border active:bg-stone-50 w-full pl-8 pr-12 sm:text-sm border-stone-300 rounded-md transition"
                 type="text"
                 placeholder="Search"
                 onChange={(e) => setSearch(e.target.value)}
               />
             </form>
           </div>
+          <button className="absolute right-0 m-4 p-2 focus:ring-orange-500/50 active:bg-stone-50 focus:ring-2 items-center justify-center flex focus:border-orange-500 border text-stone-600 border-stone-300 rounded-md shadow-sm transition">
+            <HiCog />
+          </button>
         </div>
         <div
           ref={searchSuggestionsRef}
